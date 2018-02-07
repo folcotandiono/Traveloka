5261eed154f45a7eb6cb25957064b2c27e521c22
folcotandiono <folcotandiono@gmail.com>
2018-02-07 21:00:00 +0700

    penurunan versi php agar bisa menggunakan driver sqlsrv

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-$serverName = "DESKTOP-9L9TBDK\Folco Tandiono"; //serverName\instanceName
+$serverName = "DESKTOP-9L9TBDK"; //serverName\instanceName
 
 // Since UID and PWD are not specified in the $connectionInfo array,
 // The connection will be attempted using Windows Authentication.
@@ -7,9 +7,7 @@ $connectionInfo = array( "Database"=>"Traveloka");
 $conn = sqlsrv_connect( $serverName, $connectionInfo);
 
 if( $conn ) {
-     echo "Connection established.<br />";
 }else{
-     echo "Connection could not be established.<br />";
      die( print_r( sqlsrv_errors(), true));
 }
 ?>
--- findticket.php
+++ findticket.php
@@ -7,14 +7,16 @@ $to = $_POST['to'];
 $dateFlight = $_POST['dateFlight'];
 $banyakOrang = $_POST['banyakOrang'];
 
-echo $dateFlight;
-
 $sql = "SELECT * FROM Jadwal WHERE Kota_Asal='$from' and Kota_Tujuan='$to' and Tanggal='$dateFlight'";
 $query = sqlsrv_query($conn, $sql);
 
+$array = array();
+$i = 0;
+
 while($result = sqlsrv_fetch_array($query, SQLSRV_FETCH_ASSOC)) {
-	echo $result["No_Penerbangan"];
+	$array[$i++] = $result;
 }
 
+echo json_encode($array);
 
 ?>
